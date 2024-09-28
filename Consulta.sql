26b912a2f490cdd9d806d21fd8fc2fdd6abf1a1b
JavierLutin03 <143854152+JavierLutin03@users.noreply.github.com>
2024-09-27 21:31:21 -0600

    Create Consulta.sql

---
+++ Consulta.sql
@@ -0,0 +1,7 @@
+SELECT * FROM  alumnos;
+
+SELECT * FROM alumnos WHERE columna = 'notas';
+
+SELECT COUNT(*) FROM alumnos WHERE columna = 'notas';
+
+SELECT SUM(notas) FROM alumnos WHERE columna_condicion = 'notas';
